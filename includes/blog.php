9b245f4d63a98997b8303b85e724ed97bbc0ba1f
Jean-Christophe Ballonad <jean_co@msn.com>
2022-02-08 14:30:36 +0100

    j'espere que c'est dans la branche blog mdr

---
+++ includes/blog.php
@@ -0,0 +1,42 @@
+<div>
+    <h1>BLOG</h1>
+
+
+    <ul>
+        <li>
+            <p>C’est une scène tout droit sortie d’une comédie noire qui s’est déroulée dans un bureau de poste de Carlow en Irlande le vendredi 22 janvier. Trois hommes se sont présentés au guichet pour retirer l’argent de la pension du plus âgé du trio. Âgé et visiblement mal en point puisque ses deux compagnons avaient toutes les peines du monde à le maintenir debout et qu’il semblait incapable de répondre aux questions de l’employé de la poste.</p>
+            <a href="https://www.parismatch.com/Actu/Insolite/Pour-retirer-de-l-argent-ils-se-presentent-au-guichet-avec-un-cadavre-1784235">Paris Match</a>
+        </li>
+    </ul>
+    <br>
+    <ul>
+
+        <li>
+        <p>Une escroquerie consistant à vous faire confirmer des opérations de paiement à votre insu est en pleine recrudescence. Raccrochez !</p>
+        <a href="https://www.quechoisir.org/actualite-arnaque-des-banquiers-tres-bien-imites-n98399/">Que Choisir</a>
+        </li>
+    </ul>
+        <br>
+        <ul>
+        <li>
+        <p>La France est enfin le pays des bonnes nouvelles sur le front de l’emploi, mais elle n’a pas pour autant réduit son problème du chômage de longue durée.</p>
+        <a href="https://www.lopinion.fr/economie/chomage-bonne-nouvelle-on-entre-dans-le-dur">L'opignon</a>
+        </li>
+        <br>
+        <li>
+        <p>La poussée de l'inflation va entraîner des revendications salariales plus fortes en 2022. Certains économistes redoutent qu'en retour, les hausses de salaires nourrissent celles des prix et que se mette en place une boucle inflationniste dont il sera difficile de sortir. Avec la logique suivante qui rencontre un franc succès chez les banquiers centraux depuis les années 1980 : l'inflation est comme le dentifrice . Une fois sorti du tube, on peut difficilement l'y remettre</p>
+        <a href="https://www.lesechos.fr/monde/europe/linflation-tirera-les-salaires-a-la-hausse-cette-annee-1381473">Les Echos</a>
+        </li>
+        <br>
+        <li>
+        <p>Un archipel à taille humaine classée réserve mondiale de biosphère par l’UNESCO, 50 sentiers de randonnées balisés, 400km de plages avec des eaux à 27°c en moyenne toute l’année, 6 espaces de réserves naturelles, et 99% de voyageurs qui se disent très satisfaits de leur séjour, autant d’arguments pour inciter votre clientèle à (re)découvrir Basse-Terre, Grande-Terre, Marie-Galante, Les Saintes et La Désirade</p>
+        <a href="https://www.tourmag.com/10-nouvelles-offres-d-hebergements-pour-vos-clients-dans-les-Iles-de-Guadeloupe_a111603.html">tourmag</a>
+        </li>
+        <br>
+        <li>
+        <p>Située face à la plus belle plage de la Réunion, l’Hermitage, cette magnifique villa dispose de toutes les commodités pour passer un séjour confortable sur l’île. Elle dispose d’une piscine miroir et d’un grand jardin. Dotée de 4 chambres, elle permet d'accueillir 8 voyageurs. </p>
+        <a href="https://www.admagazine.fr/adadresses/article/iles-martinique-reunion-maurice-meilleurs-airbnb-reserver-vacances-fevrier">AD Magasine</a>
+        </li>
+    </ul>
+
+</div>
